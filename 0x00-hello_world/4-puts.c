3ddcb77e330cd792ee00d8db2d756b4ac29c9b53
nellylaurene <nellylaurene10@gmail.com>
2023-06-15 14:39:36 -0700

    4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void){
-	puts("Programming is like building a multilingual puzzle");
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (indicating successful termination)
+ */
+int main (void)
+{
+        puts("Programming is like building a multilingual puzzle");
 	return 0;
 }
